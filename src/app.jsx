bbe4ac640fc5741d8a6559c9a636f279ad5724ae
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-12-09 22:09:27 +0100

    fix: fix sidebar and main section alignment

--- src/app.jsx
+++ src/app.jsx
@@ -11,25 +11,23 @@ import { SignIn } from "./presenter/signInPresenter";
 import HomePageMoviePresenter from "./presenter/homePageMoviePresenter";
 import "./index.css";
 function App() {
+  //TODO: use FLowbite as component library
   return (
-    <BrowserRouter>
-      <div style={{ display: "flex" }}>
+    <div>
+      <BrowserRouter>
         <SidebarPresenter />
-
-        <div style={{ marginLeft: 250, flexGrow: 1 }}>
-          <Routes>
-            <Route path="/" element={<HomePagePresenter />} />
-            <Route path="/aboutUs" element={<AboutUsView />} />
-            <Route path="/bookDetails" element={<BookDetailsPresenter />} />
-            <Route path="/listPage" element={<ListPagePresenter />} />
-            <Route path="/oneListPage" element={<OneListPresenter />} />
-            <Route path="/signUp" element={<SignUp />} />
-            <Route path="/signIn" element={<SignIn />} />
-            <Route path="/movie" element={<HomePageMoviePresenter />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePagePresenter />} />
+          <Route path="/aboutUs" element={<AboutUsView />} />
+          <Route path="/bookDetails" element={<BookDetailsPresenter />} />
+          <Route path="/listPage" element={<ListPagePresenter />} />
+          <Route path="/oneListPage" element={<OneListPresenter />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/movie" element={<HomePageMoviePresenter />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/view/home_page_movie/homePageView.jsx
+++ src/view/home_page_movie/homePageView.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const HomePageView = () => {
-  return <div className=" h-screen w-screen"></div>;
+  return (
+    <div className=" bg-amber-200 h-[400px] border border-red-500 w-full">
+      <h1 className="text-4xl text-center top-40 relative">
+        Welcome to the Movie Page!
+      </h1>
+    </div>
+  );
 };
 
 export default HomePageView;
