700ff48bb78a2d623c105118907869f630cf65e9
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2026-01-18 22:58:59 +0100

    refactor: migrate to vite and setup tailwind css

--- ink-insight/src/index.jsx
+++ ink-insight/src/index.jsx
@@ -4,15 +4,15 @@ import App from "./app.jsx";
 import { store } from "./store/store.js";
 import { Provider } from "react-redux";
 import FirebaseConnection from "./database/firebase.js";
-import LocalStorageConnection from "./database/localStorage.js"
+import LocalStorageConnection from "./database/localStorage.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <LocalStorageConnection/>
+      <LocalStorageConnection />
       <App />
-      <FirebaseConnection/>
+      <FirebaseConnection />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
